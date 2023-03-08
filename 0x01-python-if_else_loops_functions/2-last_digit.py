d19cb49ba5b2ca4e6197aff60b3524b26fc9c9ff
2hardcurrency <sinafavo@gmail.com>
2023-03-08 15:33:48 -0800

    my tasks

---
+++ 0x01-python-if_else_loops_functions/2-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print(f"Last digit of {number} is [:-1]
+        if [:-1] > 5:
+        print(f"and is greater than 5)
+elif [:-1] == 0:
+    print(f"and is 0")
+else:
+    print(f"and is less than 6 and not 0")
