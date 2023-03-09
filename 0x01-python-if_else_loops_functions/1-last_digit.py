73973c326d913dba4c501883d540f80694a4fe27
2hardcurrency <sinafavo@gmail.com>
2023-03-08 16:04:47 -0800

    my tasks

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is [:-1]
-        if [:-1] > 5:
-        print("and is greater than 5")
-
-        elif [:-1] == 0:
-        print("and is 0")
-
-        else [:-1] < 6 and != 0:
-        print("and is less than 6 and not 0")
+last = abs(number) % 10
+if number > 5:
+    print(f'Last digit of {number} is {last} and is greater than 5')
+elif number == 0:
+    print(f'Last digit of {number} is {last} and is 0')
+else:
+    print(f'Last digit of {number} is -{last} and is less than 6 and not 0')
--- 0x01-python-if_else_loops_functions/2-last_digit.py
+++ 0x01-python-if_else_loops_functions/2-last_digit.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is [:-1]
-        if [:-1] > 5:
-        print(f"and is greater than 5)
-elif [:-1] == 0:
-    print(f"and is 0")
-else:
-    print(f"and is less than 6 and not 0")
+for i in range(97, 123):
+    print('{}'.format(chr(i)), end='')
