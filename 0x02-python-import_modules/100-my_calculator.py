91f538d2c785a84c4106270bc24ad627362b04ca
2hardcurrency <sinafavo@gmail.com>
2023-03-13 16:47:02 -0700

    my tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-a = int(a)
-b = int(b)
-
-for the len(argv) != 3:
-    if operator == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == "-":
-        print("{} - {} = {}".format(a, b, add(a, b)))
-    elif operator == "*":
-        print("{} * {} = {}".format(a, b, add(a, b)))
-    elif operator == "/":
-        print("{} / {} = {}".format(a, b, add(a, b)))
-
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = int(a)
+    b = int(b)
+    for the len(argv) != 3:
+        if operator == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif operator == "-":
+            print("{} - {} = {}".format(a, b, add(a, b)))
+        elif operator == "*":
+            print("{} * {} = {}".format(a, b, add(a, b)))
+        elif operator == "/":
+            print("{} / {} = {}".format(a, b, add(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
 
 process finished with exit code 1
