7800bad238d2f980f9981b34a45096b2e36dce7f
2hardcurrency <sinafavo@gmail.com>
2023-03-15 16:07:33 -0700

    my tasks

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    
+    if len(my_list) = []:
+        return
+    for i in my_list:
+        new_list = my_list.sort()
+        return new_list[-1]
+
