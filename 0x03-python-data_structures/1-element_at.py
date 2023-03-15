4269db305f605d3110ef2f729bb74b370342f4ef
2hardcurrency <sinafavo@gmail.com>
2023-03-15 11:39:19 -0700

    my task

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx is negative:
+        print("None")
+    if idx is out of range:
+        print("None")
+
+    for i in my_list:
+        my_list.pop()
+        print("{:d}".format(i))
