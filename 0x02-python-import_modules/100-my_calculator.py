2db6eaabe64180677fde2bc3f71e7ad8bba1f05b
2hardcurrency <sinafavo@gmail.com>
2023-03-15 01:11:35 -0700

    my tgasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    a = int(a)
-    b = int(b)
-    for the len(argv) != 3:
-        if operator == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif operator == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif operator == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif operator == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
 
-process finished with exit code 1
+    if the len(argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    elif not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    else:
+        a = int(a)
+        b = int(b)
+    if operator == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
