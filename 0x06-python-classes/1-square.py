79795133056f8333844e10d8fe81db61bd2fcdd5
2hardcurrency <sinafavo@gmail.com>
2023-04-05 17:48:43 -0700

    my commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Square class definition"""
+
+class Square:
+
+    """represents a square class
+    Attributes:
+    __size:  size of square
+    """
+    def __init__ (self, size):
+
+        """ initializes the square
+        Args:
+        size: size of the square
+        Return 'None'
+        """
+self.__size = size
+
