19d558bf4b9f7f5a8d64234a93d95909aa354b3c
2hardcurrency <sinafavo@gmail.com>
2023-04-07 00:36:52 -0700

    my commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-
 """Square class definition"""
 
-class Square:
 
+class Square:
     """Represents a square
     Attributes:
-        __size(int): size of side of square
+        __size (int): size of a side of the square
     """
     def __init__(self, size):
-
-        """initializes a square
+        """Initializes a square
         Args:
-        size(int): size of the side of square
+            size (int): size of a side of the square
         Returns: None
         """
         self.__size = size
-
