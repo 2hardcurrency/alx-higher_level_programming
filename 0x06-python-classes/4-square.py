5d7e55d9f0aa17d4be2bc5da39ef1cbbca38f984
2hardcurrency <sinafavo@gmail.com>
2023-04-07 14:17:45 -0700

    my commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,7 +6,12 @@ class Square:
         __size (int): size of a side of the square
         """
     def __init__(self, size=0):
-        """Initializes the square."""
+        """Initializes the square.
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
 
         self.size = size
 
@@ -19,13 +24,22 @@ class Square:
 
     @property
     def size(self):
-        """getter of __size"""
+        """
+        getter of __size
+        Returns:
+            The size of the square
+            """
 
         return self.__size
 
     @size.setter
     def size(self, value):
-        """setter of __size"""
+        """setter of __size
+        Args:
+            value (int): the size of a size of the square
+        Returns:
+            None
+            """
 
 
         if type(value) is int:
