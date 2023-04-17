8ae84234c3fa633194c050c56f863d624bc46cad
2hardcurrency <sinafavo@gmail.com>
2023-04-17 09:23:46 -0700

    my commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@ Contains the function that adds to integers
 
 
 def add_integer(a, b=98):
+    """Return the addition of two integer numbers."""
     if a is not int and a is not float:
         raise TypeError("a must be an integer")
     if b is not int and b is not float:
