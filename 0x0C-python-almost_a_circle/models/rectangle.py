a21c1bc5451bb696f8e403e89d9ccd71f66b682d
2hardcurrency <sinafavo@gmail.com>
2023-04-18 16:42:40 -0700

    my commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-        super().__init__id
+        super().__init__(id)
 
     @property
     def width(self):
@@ -26,7 +26,7 @@ class Rectangle(Base):
         """setter for the attributes"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -40,7 +40,7 @@ class Rectangle(Base):
         """setter for the attributes"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
@@ -54,7 +54,7 @@ class Rectangle(Base):
         """setter for the attributes"""
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -68,7 +68,7 @@ class Rectangle(Base):
         """setter for the attributes"""
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
