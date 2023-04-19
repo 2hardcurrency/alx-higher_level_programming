4bd74a3ea1857b84c92ba0152a02384f57bc8b92
2hardcurrency <sinafavo@gmail.com>
2023-04-18 17:42:34 -0700

    commit message

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,7 +100,7 @@ class Rectangle(Base):
             **kwargs (dict): New key/value pairs of attributes.
         """
 
-            if args and len(args) != 0:
+        if args and len(args) != 0:
             a = 0
             for arg in args:
                 if a == 0:
@@ -119,20 +119,20 @@ class Rectangle(Base):
                 a += 1
 
             elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "width":
-                    self.width = v
-                elif k == "height":
-                    self.height = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+                for k, v in kwargs.items():
+                    if k == "id":
+                        if v is None:
+                            self.__init__(self. width, self.height, self.x, self.y)
+                        else:
+                            self.id = v
+                    elif k == "width":
+                        self.width = v
+                    elif k == "height":
+                        self.height = v
+                    elif k == "x":
+                        self.x = v
+                    elif k == "y":
+                        self.y = v
 
     def to_dictionary(self):
         """Return the dictionary representation of a Rectangle."""
