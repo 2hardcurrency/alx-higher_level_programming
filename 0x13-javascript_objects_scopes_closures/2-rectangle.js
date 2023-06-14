e25970e1493c3298f0571ed1b1a445e5f87ea18c
2hardcurrency <sinafavo@gmail.com>
2023-06-14 15:41:23 -0700

    javascript oop

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {}; // Return an empty object if width or height is invalid
+    if (w === 0 || h === 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
     }
 
     this.width = w;
