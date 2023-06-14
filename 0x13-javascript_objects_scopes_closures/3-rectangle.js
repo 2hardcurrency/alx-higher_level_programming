3b3e75542b84aff2f7a5257877f1d2130335b4b3
2hardcurrency <sinafavo@gmail.com>
2023-06-14 15:17:19 -0700

    javascript oop

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
+      return {}; // Return an empty object if width or height is not valid
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    if (!this.width || !this.height) {
+      console.log("Invalid rectangle"); // Print an error message if width or height is not valid
+      return;
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log("X".repeat(this.width));
+    }
+  }
+}
+
