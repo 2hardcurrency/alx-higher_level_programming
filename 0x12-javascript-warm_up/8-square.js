3f5da06550a043c99efd8e5e7462a09d628da4d3
2hardcurrency <sinafavo@gmail.com>
2023-06-13 22:07:43 -0700

    javascript warmup

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size))
-{
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let r = 0; r < size; r++) {
