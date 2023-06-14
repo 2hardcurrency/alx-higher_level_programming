577ed9cacae9794c3c7a9de380a8f84269d2eab9
2hardcurrency <sinafavo@gmail.com>
2023-06-13 21:28:03 -0700

    javascript warmup

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const count = process.argv.lenght;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : count === 4 ? 'Arguments found');
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
