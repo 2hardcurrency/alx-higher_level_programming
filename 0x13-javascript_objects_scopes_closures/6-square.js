09d5f9012296bea4e9d69cd92a2af9e818895e3e
2hardcurrency <sinafavo@gmail.com>
2023-06-15 13:46:58 -0700

    javascript warmup

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const SquareP = require('./5-square');
+
+class Square extends Square {
+  constructor () {
+  }
+  
+  charPrint(c) {
+    if (typeof 'c' === undefined) {
+      c = 'X';
+    }
+    for (i = 0; i < this.height; i++) {
+      size = '';
+      for (j = 0; j < this.width; j++) {
+	size += 'c';
+      }
+      console.log(size);
+    }
+  }
+}
+
+module.exports = Square;
