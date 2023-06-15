38a761aa56c7529bf5c8c5a79acd42165727f3b6
2hardcurrency <sinafavo@gmail.com>
2023-06-15 13:28:48 -0700

    javascript warmup

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,14 +2,9 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor(size) {
-    super(size, size); // Call the constructor of Rectangle with the size argument
+  constructor (size) {
+    super(size, size);
   }
 }
-
-// Create a new instance of Square
-const square = new Square(5);
-
-console.log(square.area());
 
 module.exports = Square;
