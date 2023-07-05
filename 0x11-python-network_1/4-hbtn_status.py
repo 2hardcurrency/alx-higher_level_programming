3be3db0769d585ae9b48caa630f63b35358de88c
2hardcurrency <sinafavo@gmail>
2023-07-05 15:14:37 -0700

    task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
 
-import request
 
 if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
