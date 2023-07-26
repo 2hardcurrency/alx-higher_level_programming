9d8fcc18ed6735c74d3033d9a2c24623349f6ec9
2hardcurrency <sinafavo@gmail.com>
2023-07-26 01:16:13 +0000

    mine

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-function readFileContent(filePath) {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(data);
-    }
+fs.readFile(process.argv[2], 'utf-8', function(err, data) {
+  console.log(err || data);
   });
-}
-
-// Usage example: node read_file.js path/to/your/file.txt
-const filePath = process.argv[2];
-readFileContent(filePath);
