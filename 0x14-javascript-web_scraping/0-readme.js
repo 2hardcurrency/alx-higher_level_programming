6256db4b883dadcda2cff4726f1b7510ac4a0bbc
2hardcurrency <sinafavo@gmail.com>
2023-07-26 01:07:09 +0000

    mine

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+function readFileContent(filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
